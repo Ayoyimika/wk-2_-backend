fd45c83bdc38dd950dd572866187e472c099e1d8
unknown <vicaava@gmail.com>
2023-02-28 13:48:41 +0100

    question 1

---
+++ index.js
@@ -0,0 +1,9 @@
+const InstagramPost = new Object();
+InstagramPost.handle = "Avastoresng";
+InstagramPost.content = "Global";
+InstagramPost.image = "https://www.instagram.com/p/CocDULFIU1y/";
+InstagramPost.views = "1k+ views";
+InstagramPost.likes = 2434;
+
+console.log(InstagramPost);
+
